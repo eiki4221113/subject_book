3def53343917b49701284bf8c376bad30b9e40fb
eiki <e.kato.sys22@morijyobi.ac.jp>
2023-06-08 12:16:14 +0900

    基礎の基礎　6/8

--- subject_book/app.py
+++ subject_book/app.py
@@ -1,1 +1,12 @@
-print("hello world!")
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def sample_top():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
