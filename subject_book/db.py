552088cabe7d47dc8dc4666962363713c0cf3055
eiki <e.kato.sys22@morijyobi.ac.jp>
2023-07-21 10:44:11 +0900

    ORDER by id を一覧に反映

--- subject_book/db.py
+++ subject_book/db.py
@@ -9,7 +9,7 @@ def get_connection():
 def select_all_books():
     connection = get_connection()
     cursor = connection.cursor()
-    sql = " select * from subject_books"
+    sql = "SELECT * FROM subject_books ORDER BY id;"
     
     cursor.execute(sql)
     rows = cursor.fetchall()
@@ -20,7 +20,7 @@ def select_all_books():
 def search_books(query):
     connection = get_connection()
     cursor = connection.cursor()
-    sql = "SELECT * FROM subject_books WHERE title LIKE %s OR author LIKE %s"
+    sql = "SELECT * FROM subject_books  WHERE title LIKE %s OR author LIKE %s"
     cursor.execute(sql, ('%' + query + '%', '%' + query + '%'))
     rows = cursor.fetchall()
     cursor.close()
